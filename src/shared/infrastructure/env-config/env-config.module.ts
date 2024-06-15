322fedbbaa64b9e93b38e6e4a2dd46a682e82927
Eduardo Coelho <eduardocoelhosilva12@gmail.com>
2024-06-14 21:57:38 -0300

    Refactoring env config module

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,7 +1,24 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { EnvConfigService } from './env-config.service';
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
   providers: [EnvConfigService]
 })
-export class EnvConfigModule {}
+export class EnvConfigModule extends ConfigModule {
+  // By default, nest only can get one file .env. To get multiple files to use multiple environments,
+  // we need do extends ConfigModule nest class to implement some configurations.
+
+  static forRoot(options?: ConfigModuleOptions): DynamicModule {
+    return super.forRoot({
+      ...options,
+      envFilePath: [
+        join(
+          __dirname,
+          `../../../../.env.${process.env.NODE_ENV}`
+        )
+      ]
+    })
+  }
+}
