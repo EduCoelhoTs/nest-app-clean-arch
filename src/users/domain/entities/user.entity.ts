56ee32016dd9d0e93ab4e2d9944f822aea91b57b
Eduardo Coelho <eduardocoelhosilva12@gmail.com>
2024-06-16 10:46:45 -0300

    User entity Constructor unit test

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,1 +1,15 @@
-export class UserEntity { }
+export type UserProps = {
+  name: string;
+  email: string;
+  password: string;
+  createdAt?: Date;
+}
+
+// the interface is the core of app in clean architecture, so we need to build only the
+// business logic of the app, not using external libraries
+export class UserEntity {
+  constructor(public readonly props: UserProps) {
+    this.props.createdAt = this.props.createdAt ?? new Date();
+  }
+
+}
